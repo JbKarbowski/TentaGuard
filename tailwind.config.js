4a41cddd2a5134a5f23bc0591867551ee3c32ade
Jakub <jbkarbowski@gmail.com>
2025-08-24 20:16:09 +0200

    background

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,12 @@ export default {
           600: "#1f62f0",
           700: "#194fbe",
           800: "#173f95",
-          900: "#142f6d"
+          900: "#142f6d",
+          background: "#0D1117",
+          surface: "#161B22",
+          accent: "#00FF9C",
+          accentLight: "#58E6C2",
+          textPrimary: "#E6EDF3"
         }
       }
     },
