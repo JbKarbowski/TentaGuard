524184a5c717b478b3cff9cbf5839ad3730919b7
JbKarbowski <42513786+JbKarbowski@users.noreply.github.com>
2025-08-24 20:25:20 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ export default {
       },
       colors: {
         brand: {
+           50: "#eef6ff",
+          100: "#dbebff",
+          200: "#bad7ff",
+          300: "#91beff",
+          400: "#5a98ff",
+          500: "#357bff",
+          600: "#1f62f0",
+          700: "#194fbe",
+          800: "#173f95",
+          900: "#142f6d",
           background: "#0D1117",
           surface: "#161B22",
           accent: "#00FF9C",
